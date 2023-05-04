71bb89bbd1a24d23242598e708258381887e74a3
Renardo Williams <rennwill@gmail.com>
2023-05-03 20:21:05 -0400

    add functions to create elements in the DOM from index.js and the database

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // gets all buttons from side nav bar in DOM
-const cake = "Our bespoke buttercream cakes add a personal touch to any occasion, whether you want to keep it simple and elegant or go for show-stopping and elaborate, we will work with you to create a bespoke design that is tailored to your specific theme for your occasion. A lot of time and attention to detail goes into creating your one of a kind master piece."
+const about = {
+    cakes : "Our bespoke buttercream cakes add a personal touch to any occasion, whether you want to keep it simple and elegant or go for show-stopping and elaborate, we will work with you to create a bespoke design that is tailored to your specific theme for your occasion. A lot of time and attention to detail goes into creating your one of a kind master piece.",
+    cupcakes : "We offer three style of cupcakes, Elegant, Luxury and Floral (All cupcakes are Jumbo). We will work with you to decide which bespoke set of cupcakes is best for you, to provide a truly customized experience for your occasion. They are great accompaniments to your showstopper cake, or perfect for gifting as a little treat on their own.",
+    cookies : "We offer three style of cookies, drop cookies, NY style cookies and Fondant covered sugar cookies. We make each cookie by hand for every order, with premium quality ingredient, attention to detail and a lot of love.",
+    brownies : "Our fudge brownies and blondies are made with premium quality, all-natural ingredients; no preservatives or stabilizers and nothing artificial. Every brownie and blondies is handcrafted with precision and love. We never want to sacrifice quality and promise to make the most irresistible and best brownies possible."
+}
+const rating = ["✯","✯✯","✯✯✯","✯✯✯✯","✯✯✯✯✯"]
+
 const buttons = document.querySelectorAll("#btn");
 // global attribute for main page area
 const main = document.querySelector(".main");
@@ -11,10 +18,14 @@ buttons.forEach(element => {element.addEventListener("click", buttonCall)});
 document.querySelector("#logo").addEventListener("click",loadHome)
 
 function buttonCall()
-{
-    requestData(this.textContent.toLowerCase());
+{    
     clearMain(load) 
-
+    let h2 = document.createElement("h2");
+    let p = document.createElement("p")
+    h2.textContent = this.textContent;
+    p.textContent = about[this.textContent.toLowerCase()];
+    load.append(h2,p)
+    requestData(this.textContent.toLowerCase());
 }
 
 function requestData(key)
@@ -26,22 +37,17 @@ function requestData(key)
 
 function generateItem(data)
 {
-    home.className ="hide";
-    
-    let h2 = document.createElement("h2");
+    home.className ="hide";    
+    let p0 = document.createElement("p");
     let p1 = document.createElement("p");
     let p2 = document.createElement("p");
     let p3 = document.createElement("p");
-
-    h2.textContent = data.name;
-    p1.textContent = data.about;
-    p2.textContent = data.price;
-    p3.textContent = data.rating;
-
-    load.append(h2, p1, p2, p3)
+    p0.textContent = data.name;
+    p1.textContent = `About: ${data.about}`;
+    p2.textContent = `Price: $${data.price}`;
+    p3.textContent = `Rating: ${rating[data.rating]}`;
+    load.append(p0, p1, p2, p3)
 }
-
-
 
 function loadHome(){ clearMain(load); home.className = ""}
 
