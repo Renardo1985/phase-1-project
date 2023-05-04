94dd7e7d809fa51fc8b0386977e4c19ae778a784
Renardo Williams <rennwill@gmail.com>
2023-05-04 17:25:15 -0400

    create order form in html add function in javascript to populate form

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-// gets all buttons from side nav bar in DOM
+
 const about = {
     cakes : "Our bespoke buttercream cakes add a personal touch to any occasion, whether you want to keep it simple and elegant or go for show-stopping and elaborate, we will work with you to create a bespoke design that is tailored to your specific theme for your occasion. A lot of time and attention to detail goes into creating your one of a kind master piece.",
     cupcakes : "We offer three style of cupcakes, Elegant, Luxury and Floral (All cupcakes are Jumbo). We will work with you to decide which bespoke set of cupcakes is best for you, to provide a truly customized experience for your occasion. They are great accompaniments to your showstopper cake, or perfect for gifting as a little treat on their own.",
     cookies : "We offer three style of cookies, drop cookies, NY style cookies and Fondant covered sugar cookies. We make each cookie by hand for every order, with premium quality ingredient, attention to detail and a lot of love.",
     brownies : "Our fudge brownies and blondies are made with premium quality, all-natural ingredients; no preservatives or stabilizers and nothing artificial. Every brownie and blondies is handcrafted with precision and love. We never want to sacrifice quality and promise to make the most irresistible and best brownies possible."
 }
+//ratings array
 const rating = ["✯","✯✯","✯✯✯","✯✯✯✯","✯✯✯✯✯"]
 
+// gets all buttons from nav bar in DOM
 const buttons = document.querySelectorAll("#btn");
 // global attribute for main page area
 const main = document.querySelector(".main");
@@ -19,7 +21,7 @@ document.querySelector("#logo").addEventListener("click",loadHome)
 
 function buttonCall()
 {    
-    clearMain(load) 
+    clearNode(load) 
     let h2 = document.createElement("h2");
     let p = document.createElement("p")
     h2.textContent = this.textContent;
@@ -32,11 +34,16 @@ function requestData(key)
 {
     fetch(`http://localhost:3000/${key}`)
     .then(data => data.json())
-    .then(item => item.forEach(item =>generateItem(item))) 
+    .then((item) => {
+                        item.forEach(item =>generateItem(item));
+                        populateForm(item);
+                    })
+    .catch(function(error){alert(error)});
 }
 
 function generateItem(data)
 {
+    
     home.className ="hide";    
     let p0 = document.createElement("p");
     let p1 = document.createElement("p");
@@ -49,10 +56,22 @@ function generateItem(data)
     load.append(p0, p1, p2, p3)
 }
 
+function populateForm(data){    
+    const select = document.getElementById("item-name");
+    clearNode(select)
+    
+    data.forEach( data => {
+        let option = document.createElement("option");
+        option.value = data.name;
+        option.textContent = data.name;
+        select.appendChild(option);
+    });
+}
+
 function loadHome(){ clearMain(load); home.className = ""}
 
-//function used to clear main body area in DOM
-function clearMain(node) {
+//function used to clear nodes in DOM
+function clearNode(node) {
     while (node.firstChild) {
        node.removeChild(node.firstChild);
     }
