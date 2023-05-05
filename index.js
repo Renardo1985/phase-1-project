c9a9cdb5acee62e6239b02d5042ca07036e03e6a
Renardo Williams <rennwill@gmail.com>
2023-05-04 21:26:40 -0400

    add item rating system

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-
+//array of object for Cart
+const cart = [];
 const about = {
     cakes : "Our bespoke buttercream cakes add a personal touch to any occasion, whether you want to keep it simple and elegant or go for show-stopping and elaborate, we will work with you to create a bespoke design that is tailored to your specific theme for your occasion. A lot of time and attention to detail goes into creating your one of a kind master piece.",
     cupcakes : "We offer three style of cupcakes, Elegant, Luxury and Floral (All cupcakes are Jumbo). We will work with you to decide which bespoke set of cupcakes is best for you, to provide a truly customized experience for your occasion. They are great accompaniments to your showstopper cake, or perfect for gifting as a little treat on their own.",
@@ -7,6 +8,8 @@ const about = {
 }
 //ratings array
 const rating = ["✯","✯✯","✯✯✯","✯✯✯✯","✯✯✯✯✯"]
+
+
 
 // gets all buttons from nav bar in DOM
 const buttons = document.querySelectorAll("#btn");
@@ -19,44 +22,76 @@ const load = document.querySelector("#load");
 buttons.forEach(element => {element.addEventListener("click", buttonCall)});
 document.querySelector("#logo").addEventListener("click",loadHome)
 
+//call back function for clicks on Navigation bar
 function buttonCall()
 {    
     clearNode(load) 
     let h2 = document.createElement("h2");
     let p = document.createElement("p")
     h2.textContent = this.textContent;
-    p.textContent = about[this.textContent.toLowerCase()];
-    load.append(h2,p)
-    requestData(this.textContent.toLowerCase());
+   
+    if (this.textContent === "Cart")
+    {        
+        p.textContent = `You have ${cart.length} item(s) in your cart`
+        load.append(h2,p)
+    }
+    else{
+        
+        p.textContent = about[this.textContent.toLowerCase()];
+        load.append(h2,p)
+        requestData(this.textContent.toLowerCase());
+    }
+    
 }
-
+//main fetch to read from db.json
 function requestData(key)
 {
+    
     fetch(`http://localhost:3000/${key}`)
     .then(data => data.json())
     .then((item) => {
-                        item.forEach(item =>generateItem(item));
-                        populateForm(item);
+                        item.forEach(item =>generateItem(item,key));
+                        //populateForm(item);
                     })
     .catch(function(error){alert(error)});
 }
+function updateRating(item,key){
+    fetch(`http://localhost:3000/${key}/${item.id}`, {method: "PATCH",headers:{"Content-Type": "application/json",Accept: "application/json"},body: JSON.stringify({"rating":item.rating})})
+  
+  }
 
-function generateItem(data)
-{
-    
-    home.className ="hide";    
+function generateItem(data,key)
+{    
+        
     let p0 = document.createElement("p");
     let p1 = document.createElement("p");
     let p2 = document.createElement("p");
-    let p3 = document.createElement("p");
+    let rate = document.createElement("button");
     let img = document.createElement("img");
+    let add = document.createElement("button")   
 
     p0.textContent = data.name;
     p1.textContent = `About: ${data.about}`;
+    p1.style.visibility = "hidden";
     p2.textContent = `Price: $${data.price}`;
-    p3.textContent = `Rating: ${rating[data.rating]}`;
+    rate.textContent = `Rating:${rating[data.rating]}`;    
     img.src = data.image;
-    load.append(p0, img, p1, p2, p3)
+    //on mouseover show item description
+    img.addEventListener("mouseover",()=>{p1.style.visibility ="";})
+    img.style.height = "12rem"
+    img.addEventListener("mouseleave",()=>{p1.style.visibility ="hidden";})
+    rate.addEventListener("click",() => 
+    {
+        if (data.rating < 4){data.rating += 1}
+        else {data.rating = 0}
+        rate.textContent = `Rating:${rating[data.rating]}`;
+        updateRating(data,key)
+
+    })
+    add.textContent = "Add to Cart"
+    //when add to cart is clicked adds item to array cart
+    add.addEventListener("click",() => {cart.push(data); console.log(cart)})
+    load.append(p0, img,p1,rate, p2,add)
 }
 
 function populateForm(data){    
