9085aa14045659ed68fe6fea3f4d61a59f3d3534
Renardo Williams <rennwill@gmail.com>
2023-05-09 22:00:37 -0400

    edit index.html and style.css to meet W3C validation

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 https://www.w3schools.com/howto/howto_css_fixed_sidebar.asp
 https://www.javascripttutorial.net/dom/manipulating/remove-all-child-nodes/
+https://uxplanet.org/how-to-create-a-shopping-cart-ui-using-html-css-e5db3cd55aa0
+https://developer.mozilla.org/en-US/docs/Web/CSS/display
--- index.js
+++ index.js
@@ -108,13 +108,13 @@ function generateItem(data, key) {
     })
     add.textContent = "Add to Cart"
     //when add to cart is clicked adds item to array cart 
-    add.addEventListener("click", () => {    
+    add.addEventListener("click", () => {
         let index = -1
         for (let i = 0; i < cart.length; i++) {
-            if (cart[i].name === data.name){index=i}            
-        }        
-        if (index>= 0) { cart[index].quantity += 1; }//if item already exist in array +1 quantity
-        else { data.quantity = 1; cart.push(data);  } // if item does not exist in array add item to array
+            if (cart[i].name === data.name) { index = i }
+        }
+        if (index >= 0) { cart[index].quantity += 1; }//if item already exist in array +1 quantity
+        else { data.quantity = 1; cart.push(data); } // if item does not exist in array add item to array
     })
     container.append(h, img, p1, rate, p2, add)
     load.append(container)
@@ -128,7 +128,6 @@ function loadCart() {
     load.append(h2, p)
     //loops throught the cart array
     cart.forEach((element, index) => {
-
         let item = document.createElement("div");
         let p1 = document.createElement("p");
         let p2 = document.createElement("p");
@@ -138,34 +137,31 @@ function loadCart() {
         let pQ = document.createElement("div")
         let add = document.createElement("button")
         let remove = document.createElement("button")
-        
         selector.classList = "addremove";
         pQ.classList = "priceQ"
         item.classList = "cart-item"
         p1.classList = "cartP1"
         img.classList = "cart-img"
-
-
         img.src = element.image;
         p1.textContent = `${element.name}`;
         p2.textContent = `Price: $${element.price}`;
         p3.textContent = `Qty: ${element.quantity}`;
         add.textContent = "add";
         remove.textContent = "remove"
-
-        remove.addEventListener("click", () => { 
-            if(cart[index].quantity >1){
-                cart[index].quantity -=1
+        remove.addEventListener("click", () => {
+            if (cart[index].quantity > 1) {
+                cart[index].quantity -= 1
                 loadCart()
             }
-            else{
-            cart.splice(index, 1); loadCart(); }
-            });
-        add.addEventListener("click",()=>{ cart[index].quantity +=1; loadCart()});
+            else {
+                cart.splice(index, 1); loadCart();
+            }
+        });
+        add.addEventListener("click", () => { cart[index].quantity += 1; loadCart() });
 
-        selector.append(add,remove)
-        pQ.append(p2,p3)
-        item.append(img, p1,pQ, selector,)
+        selector.append(add, remove)
+        pQ.append(p2, p3)
+        item.append(img, p1, pQ, selector,)
         load.append(item)
     })
     //Renders a check out button once there are items in the cart
@@ -203,7 +199,7 @@ function checkOutForm() {
 function reduceCart(key) {
     if (key === 1) {
         const sum = cart.reduce((accumulator, currentValue) => {
-            return accumulator + (currentValue.price*currentValue.quantity);
+            return accumulator + (currentValue.price * currentValue.quantity);
         }, 0);
         return sum
     }
@@ -213,7 +209,7 @@ function reduceCart(key) {
         }, 0);
         return sum;
     }
-  
+
 }
 
 
